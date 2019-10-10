64286c3ab240378c39c4c86b899249292037963e
Steeven Regnault <steeven.regnault@epitech.eu>
2019-10-10 16:42:30 +0200

    [+] NavBar with style

--- App.js
+++ App.js
@@ -1,36 +1,103 @@
 import React from 'react';
+import {Text} from 'react-native'
 import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 import LoginContainer from "./src/views/Login";
 import HomeContainer from "./src/views/Home";
 import UserContainer from "./src/views/User";
 import FavoriteContainer from "./src/views/Favorite";
 import UploadContainer from "./src/views/Upload";
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-
-const AppNavigator = createStackNavigator(
-    {
-        Login: LoginContainer,
-        Home: HomeContainer,
-        User: UserContainer,
-        Favorite: FavoriteContainer,
-        Upload: UploadContainer,
-    },
-    {
-        defaultNavigationOptions: {
-            headerTitle: "Ouistigram",
-            headerTintColor: '#fff',
-            headerStyle: {
-                backgroundColor: '#000',
-            },
-        },
-    }
-);
-
-const AppContainer = createAppContainer(AppNavigator);
 
 export default class App extends React.Component {
     render() {
         return <AppContainer/>;
     }
 }
+
+/*const bottomTabNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeContainer,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon.Button
+                        name={"home"}
+                        color={tintColor}
+                        size={25}
+                        backgroundColor="transparent"
+                    />
+                )
+            }
+        },
+        Upload: {
+            screen: UploadContainer,
+            navigationOptions: {
+                tabBarIcon: ({ tintColor }) => (
+                    <Icon.Button
+                        name={"plus-square"}
+                        color={tintColor}
+                        size={25}
+                        backgroundColor="transparent"
+                    />
+                )
+            }
+        },
+    },
+    {
+        initialRouteName: 'Home',
+        tabBarOptions: {
+            activeTintColor: '#eb6e3d'
+        }
+    }
+);*/
+
+
+const bottomTabNavigator = createBottomTabNavigator(
+    {
+        Home: {
+            screen: HomeContainer,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name={"home"} color={tintColor} size={25}/>
+                )
+            }
+        },
+        Upload: {
+            screen: UploadContainer,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name={"plus-square"} color={tintColor} size={25}/>
+                )
+            }
+        },
+        Favorite: {
+            screen: FavoriteContainer,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name={"heart"} color={tintColor} size={25}/>
+                )
+            }
+        },
+        User: {
+            screen: UserContainer,
+            navigationOptions: {
+                tabBarIcon: ({tintColor}) => (
+                    <Icon name={"user"} color={tintColor} size={25}/>
+                )
+            }
+        },
+    },
+    {
+        initialRouteName: 'Home',
+        tabBarOptions: {
+            activeTintColor: 'white',
+            inactiveTintColor: 'white',
+            showLabel: false,
+            style: {backgroundColor: 'hsl(218,7%,21%)'}
+        }
+    }
+);
+
+const AppContainer = createAppContainer(bottomTabNavigator);
