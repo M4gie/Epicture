5ef46ae207b14a91f44647ba15cf799757ef0ce5
Steeven Regnault <steeven.regnault@epitech.eu>
2019-10-16 17:02:42 +0200

    [+] Use flatList to display favorites

--- src/views/Favorite.js
+++ src/views/Favorite.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {Image, StyleSheet, View} from 'react-native';
+import {FlatList, Image, StyleSheet, TouchableOpacity, View} from 'react-native';
 import LayoutContainer from '../components/layout/Layout';
-import GridList from 'react-native-grid-list';
-import SquareImage from '../components/image/SquareImage';
 import AccountServices from "../services/account";
 
 class FavoriteContainer extends React.Component {
@@ -13,12 +11,16 @@ class FavoriteContainer extends React.Component {
         }
     }
 
-    renderItem = ({item, index}) => <SquareImage item={item} index={index}/>;
+    /*renderItem = ({item, index}) => <SquareImage item={item} index={index}/>;*/
 
-/*    componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS): void {
-        console.log(this.state.favorites)
-    }*/
-
+    renderItem(item) {
+        return (
+            <TouchableOpacity
+                style={{flex:1/3, aspectRatio:1}}>
+                <Image style={{flex: 1}} resizeMode='cover' source={item.thumbnail}/>
+            </TouchableOpacity>
+        )
+    }
 
     componentDidMount(): void {
         AccountServices.favorites(0)
@@ -26,7 +28,7 @@ class FavoriteContainer extends React.Component {
                 let pictures = response.data.map((fav) => {
                     return {
                         thumbnail: {
-                            uri: fav.images[0].link
+                            uri: fav.images[0].link,
                         }
                     };
                 });
@@ -43,13 +45,18 @@ class FavoriteContainer extends React.Component {
             <LayoutContainer title={'Favoris'} font={''} fontSize={20}>
                 <View style={styles.main_container}>
                     <View style={styles.pictures}>
-                        <GridList
+                        {/*<GridList
                             showSeparator
                             separatorBorderColor={'hsl(220,13%,14%)'}
                             separatorBorderWidth={1}
                             data={this.state.favorites}
                             numColumns={3}
                             renderItem={(item, index) => this.renderItem(item, index)}
+                        />*/}
+                        <FlatList
+                            numColumns={3}
+                            data={this.state.favorites}
+                            renderItem={({ item }) => this.renderItem(item)}
                         />
                     </View>
                 </View>
