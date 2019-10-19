52d30164cf6168fea247a2d66952ff7f13efae0d
Steeven Regnault <steeven.regnault@epitech.eu>
2019-10-19 22:08:34 +0200

    [=] Image view

--- src/components/image/ImageView.js
+++ src/components/image/ImageView.js
@@ -1,66 +1,71 @@
 import React from 'react';
-import {StyleSheet, View, Image, Text} from 'react-native';
+import { StyleSheet, View, Image, Text } from 'react-native';
 import stylesheet from '../../styles/styles';
 import CircleImage from './CircleImage';
 import Favorite from '../vote/Favorite';
 import UpDown from '../vote/UpDown';
-import Lightbox from 'react-native-lightbox';
 
-class ImageView extends React.Component {
-  render() {
-    const image = this.props.image;
+const ImageView = props => {
+  const { image } = props;
 
-    return (
-      <View style={styles.main_container}>
-        <View style={styles.topview}>
-          <View style={{width: 60, padding: 5}}>
-            <CircleImage size={60} pic={image.profilepic ? image.profilepic : 'https://i.imgur.com/mCHMpLT.png?3'} />
-          </View>
-          <View
-            style={{
-              flex: 1,
-              height: '100%',
-              padding: 5,
-            }}>
-            <Text style={[stylesheet.bold, stylesheet.white]}>
-              {image.title ? image.title : 'Pas de titre'}
-            </Text>
-            <Text style={[{fontSize: 12}, stylesheet.grey]}>
-              {image.username}
-            </Text>
-          </View>
+  return (
+    <View style={styles.main_container}>
+      <View style={styles.topview}>
+        <View style={{ width: 60, padding: 5 }}>
+          <CircleImage
+            size={60}
+            pic={
+              image.profilepic
+                ? image.profilepic
+                : 'https://i.imgur.com/mCHMpLT.png?3'
+            }
+          />
         </View>
-
-        <View style={styles.image}>
-          <Image source={{uri: image.image}} style={{flex: 1}} />
-        </View>
-
-        <View style={styles.bottomview}>
-          <View style={{flexDirection: 'row', marginBottom: 5}}>
-            <UpDown
-              imageHash={image.id}
-              ups={image.ups}
-              downs={image.downs}
-              vote={image.vote}
-            />
-            <Favorite
-              iconSize={20}
-              favorite={image.favorite}
-              imageHash={image.id}
-            />
-          </View>
-          <Text numberOfLines={2} style={[stylesheet.bold, stylesheet.white]}>
+        <View
+          style={{
+            flex: 1,
+            height: '100%',
+            padding: 5,
+          }}
+        >
+          <Text style={[stylesheet.bold, stylesheet.white]}>
+            {image.title ? image.title : 'Pas de titre'}
+          </Text>
+          <Text style={[{ fontSize: 12 }, stylesheet.grey]}>
             {image.username}
-            <Text style={{fontWeight: '200'}}>
-              {' '}
-              {image.description ? image.description : 'Pas de description'}
-            </Text>
           </Text>
         </View>
       </View>
-    );
-  }
-}
+
+      <View style={styles.image}>
+        <Image source={{ uri: image.image }} style={{ flex: 1 }} />
+      </View>
+
+      <View style={styles.bottomview}>
+        <View style={{ flexDirection: 'row', marginBottom: 5 }}>
+          <UpDown
+            imageHash={image.id}
+            ups={image.ups}
+            downs={image.downs}
+            vote={image.vote}
+          />
+          <Favorite
+            iconSize={20}
+            favorite={image.favorite}
+            imageHash={image.id}
+          />
+        </View>
+        <Text numberOfLines={2} style={[stylesheet.bold, stylesheet.white]}>
+          {image.username}
+          <Text style={{ fontWeight: '200' }}>
+            {' '}
+            {image.description ? image.description : 'Pas de description'}
+          </Text>
+        </Text>
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   main_container: {
@@ -74,7 +79,7 @@ const styles = StyleSheet.create({
     margin: 10,
     flexDirection: 'row',
     justifyContent: 'center',
-    alignItems: 'center',
+    alignItems: 'center'
   },
   image: {
     height: 300,
