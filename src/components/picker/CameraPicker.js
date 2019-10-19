af15ce646d9f5fc79f7e6065cd8d681fd01700ae
Steeven Regnault <steeven.regnault@epitech.eu>
2019-10-19 22:04:34 +0200

    [=] Refractor Gallery and Camera picker components

--- src/components/picker/CameraPicker.js
+++ src/components/picker/CameraPicker.js
@@ -1,42 +1,39 @@
 // CameraPicker.js
 
 import React from 'react';
-import { Button, StyleSheet, View } from 'react-native';
+import { Button } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import ImageServices from "../../services/image";
 
-class CameraPicker extends React.Component {
-  render() {
-    return (
-      <Button
-        title={'Prendre une photo'}
-        onPress={() =>
-          ImagePicker.openCamera({
-            width: 300,
-            height: 400,
-            cropping: true,
-            includeBase64: true,
+const CameraPicker = props => {
+  const { handleUploadLoading } = props;
+  return (
+    <Button
+      title={'Prendre une photo'}
+      onPress={() =>
+        ImagePicker.openCamera({
+          width: 300,
+          height: 400,
+          cropping: true,
+          includeBase64: true,
+        })
+          .then(image => {
+            this.props.handleUploadLoading(true);
+            ImageServices.upload(image)
+              .then(() => {
+                handleUploadLoading(false);
+              })
+              .catch(error => {
+                handleUploadLoading(false);
+                console.log(error);
+              });
           })
-            .then(image => {
-              this.props.handleUploadLoading(true);
-              ImageServices.upload(image)
-                .then(() => {
-                  this.props.handleUploadLoading(false);
-                })
-                .catch(error => {
-                  this.props.handleUploadLoading(false);
-                  console.log(error);
-                });
-            })
-            .catch(err => {
-              console.log('openCamera catch' + err.toString());
-            })
-        }
-      />
-    );
-  }
-}
-
-const styles = StyleSheet.create({});
+          .catch(err => {
+            console.log('openCamera catch' + err.toString());
+          })
+      }
+    />
+  );
+};
 
 export default CameraPicker;
--- src/components/picker/GalleryPicker.js
+++ src/components/picker/GalleryPicker.js
@@ -1,42 +1,39 @@
 // GalleryPicker.js
 
 import React from 'react';
-import { Button, StyleSheet } from 'react-native';
+import { Button } from 'react-native';
 import ImagePicker from 'react-native-image-crop-picker';
 import ImageServices from "../../services/image";
 
-class GalleryPicker extends React.Component {
-  render() {
-    return (
-      <Button
-        title={'Choisir une photo de la galerie'}
-        onPress={() => {
-          ImagePicker.openPicker({
-            width: 300,
-            height: 400,
-            cropping: true,
-            includeBase64: true,
+const GalleryPicker = props => {
+  const { handleUploadLoading } = props;
+  return (
+    <Button
+      title={'Choisir une photo de la galerie'}
+      onPress={() => {
+        ImagePicker.openPicker({
+          width: 300,
+          height: 400,
+          cropping: true,
+          includeBase64: true,
+        })
+          .then(image => {
+            this.props.handleUploadLoading(true);
+            ImageServices.upload(image)
+              .then(() => {
+                handleUploadLoading(false);
+              })
+              .catch(error => {
+                console.log(error);
+                handleUploadLoading(false);
+              });
           })
-            .then(image => {
-              this.props.handleUploadLoading(true);
-              ImageServices.upload(image)
-                .then(() => {
-                  this.props.handleUploadLoading(false);
-                })
-                .catch(error => {
-                  console.log(error);
-                  this.props.handleUploadLoading(false);
-                });
-            })
-            .catch(error => {
-              console.log(error);
-            });
-        }}
-      />
-    );
-  }
-}
-
-const styles = StyleSheet.create({});
+          .catch(error => {
+            console.log(error);
+          });
+      }}
+    />
+  );
+};
 
 export default GalleryPicker;
